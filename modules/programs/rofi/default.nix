e59e8b1f22744672b5c4b277c400bd83ffe8d100
unknown <matvey.dubkov.05@gmail.com>
2026-07-22 04:29:56 +0300

    add: rofi theme config

--- modules/programs/rofi/default.nix
+++ modules/programs/rofi/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   host,
@@ -6,6 +7,8 @@
 }:
 let 
   inherit (import ../../../hosts/${host}/variables.nix) terminal;
+  inherit (lib) getExe;
+  inherit (config.lib.formats.rasi) mkLiteral;
 in
 {
   home-manager.sharedModules = [
@@ -17,16 +20,71 @@ in
           modi = "drun,run,window";
           lines = 5;
           show-icons = true;
-          terminal = "${terminal}";
+          terminal = "${getExe pkgs.${terminal}}";
           drun-display-format = "{icon} {name}";
           location = 0;
           disable-history = false;
           hide-scrollbar = true;
           display-drun = "   Apps ";
           display-run = "   Run ";
-          display-window = "   window";
-          display-Network = " 󰤨  Network";
+          display-window = "   Window";
           sidebar-mode = true;
+
+          theme = {
+            "window" = {
+              height = 360;
+              border = 3;
+            };
+
+            "inputbar" = {
+              children = map mkLiteral ["prompt" "entry"];
+              border-radius = 5;
+              padding = 2;
+            };
+
+            "prompt" = {
+              padding = 6;
+              border-radius = 3;
+              margin = mkLiteral "20px 0px 0px 20px";
+            };
+
+            "textbox-prompt-colon" = {
+              expand = false;
+              str = ":";
+            };
+
+            "entry" = {
+              padding = 6;
+              margin = mkLiteral "20px 0px 0px 10px";
+              placeholder = "Search";
+            };
+
+            "listview" = {
+              border = mkLiteral "0px 0px 0px";
+              padding = mkLiteral "6px 0px 0px";
+              margin = mkLiteral "10px 0px 0px 20px";
+              columns = 2;
+            };
+
+            "element" = {
+              padding = 5;
+            };
+
+            "element-icon" = {
+              size = 25;
+            };
+
+            "mode-switcher" = {
+              spacing = 0;
+            };
+
+            "button" = {
+              padding = 10;
+              vertical-align = 0.5; 
+              horizontal-align = 0.5;
+            };
+          };
+
         };
 
       };
