70e8fa92c4d84fcf10aac2bd10443f28392645dc
unknown <matvey.dubkov.05@gmail.com>
2026-07-22 21:04:51 +0300

    add: nvim plugins

--- modules/programs/nixvim/keybinds.nix
+++ modules/programs/nixvim/keybinds.nix
@@ -5,37 +5,40 @@
     ### Telescope ###
     {
       action = "<cmd>Telescope find_files<CR>";
-      key = "<leader>ff";
+      key = "<leader>ff"; # Поиск файла по имени
     }
 
     {
       action = "<cmd>Telescope live_grep<CR>";
-      key = "<leader>ft";
+      key = "<leader>ft"; # Поиск по содержимому в файлах
     }
 
     {
       action = "<cmd>Telescope buffers<CR>";
-      key = "<leader>fb";
+      key = "<leader>fb"; # Поиск по буферам
     }
 
     {
       action = "<cmd>Telescope help_tags<CR>";
-      key = "<leader>fh";
+      key = "<leader>fh"; # Поиск по help
     }
 
     {
       action = "<cmd>Telescope git_branches<CR>";
-      key = "<leader>gb";
+      key = "<leader>gb"; # Ветки 
     }
 
     {
       action = "<cmd>Telescope git_commits<CR>";
-      key = "<leader>gc";
+      key = "<leader>gc"; # Коммиты
     }
 
     {
       action = "<cmd>Telescope git_status<CR>";
-      key = "<leader>gs";
+      key = "<leader>gs"; # Статус
     }
+
+
+    
   ];
 }
--- modules/programs/nixvim/plugins/default.nix
+++ modules/programs/nixvim/plugins/default.nix
@@ -2,5 +2,7 @@
   imports = [
     ./telescope.nix
     ./lualine.nix
+    ./treesitter.nix
+    ./dashboard.nix
   ];
 }
